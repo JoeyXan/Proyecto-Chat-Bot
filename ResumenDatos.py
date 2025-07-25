20f99e60ab831eb78b44312e6bed431472b5bc4f
JoeyXan <joseph.escalante.est@tecazuay.edu.ec>
2025-07-24 23:06:47 -0500

    Update ResumenDatos.py

--- ResumenDatos.py
+++ ResumenDatos.py
@@ -313,8 +313,8 @@ st.markdown("""
 def load_data():
     try:
         # Leer archivos Excel directamente desde el repo (GitHub o local)
-        df_encuesta = pd.read_excel r("Encuesta para el proyecto _Asistente Institucional_ (respuestas).xlsx")
-        df_inscripcion = pd.read_excel r("Inscripción ImpulsaT - TECAZUAY (respuestas) (1).xlsx")
+        df_encuesta = pd.read_excel ("Encuesta para el proyecto _Asistente Institucional_  (respuestas)")
+        df_inscripcion = pd.read_excel ("Inscripción ImpulsaT - TECAZUAY (respuestas) (1).xlsx")
 
 
         return df_encuesta, df_inscripcion
